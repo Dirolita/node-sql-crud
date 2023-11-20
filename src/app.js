492b1a1c1acf8fee0dc0fd503596a903acde2f99
Diana Carolina Serrano <dianacaroserranot@gmail.com>
2023-11-20 09:24:33 -0500

    Coneccion a db, cofiguracion de engine hbs

--- src/app.js
+++ src/app.js
@@ -13,9 +13,24 @@ app.listen(app.get('port'), ()=>{
     console.log('listening on port', app.get('port'));
 });
 
+//2: Establecer coneccion con la base de datos
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'SQL.2023',
     database: 'inventario_p' // Nombre de tu base de datos
   });
+
+//3: Configurando vistas
+
+app.set('views', __dirname + '/views')
+app.engine('.hbs',engine({
+    extname:'.hbs'
+}))
+app.set('views engine','hbs');
+
+//4: Definir ruta principal
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
